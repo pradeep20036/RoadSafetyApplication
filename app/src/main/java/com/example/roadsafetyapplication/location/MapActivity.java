b5ceb006d05051d518fa3827b8f6c387be9be2a5
Pradeep Kumar <pradeep20036@iiitd.ac.in>
2021-03-07 10:35:59 +0530

    location is coming at interval

---
+++ app/src/main/java/com/example/roadsafetyapplication/location/MapActivity.java
@@ -0,0 +1,133 @@
+package com.example.roadsafetyapplication.location;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.roadsafetyapplication.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION=1;
+    private static final int RESULT_CODE = 11;
+    Handler h=new Handler();
+    TextView tv_speed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        tv_speed=findViewById(R.id.tv_speed);
+
+        findViewById(R.id.bt_startlocationupdate).setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION
+        )!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MapActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_LOCATION_PERMISSION);
+        }
+        else
+        {
+            startLocationService();
+        }
+    }
+  }
+        );
+
+        findViewById(R.id.bt_stoplocationupdate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopLocationService();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode== REQUEST_CODE_LOCATION_PERMISSION && grantResults.length >0){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                startLocationService();
+            }
+            else{
+                Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+
+    }
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        if (activityManager != null) {
+            for (ActivityManager.RunningServiceInfo service :
+                    activityManager.getRunningServices(Integer.MAX_VALUE)) {
+                if (LocationService.class.getName().equals(service.service.getClassName())) {
+                    if (service.foreground) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+
+    }
+
+    private void startLocationService(){
+
+        if(!isLocationServiceRunning()){
+            Intent intent=new Intent(getApplicationContext(),LocationService.class);
+
+            intent.setAction(Constants.ACTION_START_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this,"Location service started",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void stopLocationService(){
+
+        if(!isLocationServiceRunning()){
+            Intent intent=new Intent(getApplicationContext(),LocationService.class);
+            intent.setAction(Constants.ACTION_STOP_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this,"Location service stopped",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
