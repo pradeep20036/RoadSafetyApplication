072aed200d2af7249b6a87a612b88372936fc0e9
Anshuman Parashar <anshuman20056@iiitd.ac.in>
2021-03-17 13:32:33 +0530

    SPLASH SCREEN ADDED

--- app/src/main/java/com/example/roadsafetyapplication/MainActivity.java
+++ app/src/main/java/com/example/roadsafetyapplication/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.roadsafetyapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.roadsafetyapplication.signup.SignUpActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    FirebaseUser firebaseUser;
+    private static int SPLASH_SCREEN_TIME_OUT=2500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (firebaseUser != null && firebaseUser.isEmailVerified()) {
+                    startActivity(new Intent(getApplicationContext(), BottomNavigationActivity.class));
+                }
+                else{
+                    startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+                }
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
